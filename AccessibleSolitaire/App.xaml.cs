049883204d53134b455601fc831d005ae58040e3
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2025-05-28 14:06:26 +0100

    On Windows, go back to saving session on window Destroying

--- AccessibleSolitaire/App.xaml.cs
+++ AccessibleSolitaire/App.xaml.cs
@@ -19,12 +19,16 @@ namespace Sa11ytaire4All
             window.MinimumHeight = 400;
 #endif
 
-            // On Windows, we could use Destroying() here, but that's no guaranteed on mobile apparently,
-            // (and didn't work when tested on Android). So use Deactivating instead. The work being done
-            // here should be completed pretty quick.
+// On Windows, we use Destroying() here, but that's no guaranteed on mobile apparently,
+// (and didn't work when tested on Android). So use Deactivating instead. The work being
+// done here should be completed pretty quick.
+#if WINDOWS
+            window.Destroying += (s, e) =>
+#else
             window.Deactivated += (s, e) =>
+#endif
             {
-                Debug.WriteLine("Soliatire: App window deactivating.");
+                Debug.WriteLine("Soliatire: App window Destroying or Deactivating.");
 
                 var singleton = Sa11ytaire4All.MainPage.MainPageSingleton;
                 if (singleton != null)
