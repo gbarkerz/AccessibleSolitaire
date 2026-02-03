4a50ff4bd4b8c001616c7cdcc8cc4186880df0b1
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2026-02-03 13:40:29 +0000

    Update to version number 3.15.

--- AccessibleSolitaire/Strings/StringResources.Designer.cs
+++ AccessibleSolitaire/Strings/StringResources.Designer.cs
@@ -187,7 +187,7 @@ namespace Sa11ytaire4All.Strings {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Accessible Solitaire V3.14.
+        ///   Looks up a localized string similar to Accessible Solitaire V3.15.
         /// </summary>
         internal static string AccessibleSolitaireVersion {
             get {
